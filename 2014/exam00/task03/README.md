2924270df95ed3a2b41238e5ae04ec79d8b136d6
Vitaly Khabarov <vkhabarov@s-terra.com>
2014-10-04 20:26:22 +0400

    Merged step 3 and step4 Updated how-to

--- 2014/exam00/task03/README.md
+++ 2014/exam00/task03/README.md
@@ -1,18 +1,53 @@
 # Яндекс КИТ 2014.
 ## Регистрационные задания. Задание #3.
 
-1. Загрузить yakit-z01, зайти под toor и выполнить команды
+### Краткая инструкция по настройке
+1. Загрузите yakit-z01, зайдите под учетной записью toor и выполните команды
 ```
 sudo su
+apt-get update
+apt-get install curl
 source <(curl -s https://raw.githubusercontent.com/KrylCW/YaKit/master/2014/exam00/task03/step01.sh)
 ```
-2. Перезагрузить yakit-z01 в среду Debian/Ubuntu LiveCD и выполнить команды
+2. Перезагрузить yakit-z01 в среду Debian/Ubuntu LiveCD и выполнить команду
 ```
 sudo su
 source <(curl -s https://raw.githubusercontent.com/KrylCW/YaKit/master/2014/exam00/task03/step02.sh)
 ```
-3. Перезапустить yakit-z01 и выполнить команды
+3. Перезапустить yakit-z01 и выполнить команду
 ```
 sudo su
-source <(curl -s https://raw.githubusercontent.com/KrylCW/YaKit/master/2014/exam00/task/step03.sh)
+source <(curl -s https://raw.githubusercontent.com/KrylCW/YaKit/master/2014/exam00/task03/step03.sh)
 ```
+4. Зайдите по http на адрес yakit-z01 и завершите установку Wordpress
+5. Измените количество RAM yakit-z03 на 768 МБ, загрузите yakit-z03 и выполните команды
+```
+sudo su
+apt-get update
+apt-get install curl
+source <(curl -s https://raw.githubusercontent.com/KrylCW/YaKit/master/2014/exam00/task03/step05.sh)
+```
+6. На yakit-z01 выполните команду
+```
+sudo su
+source <(curl -s https://raw.githubusercontent.com/KrylCW/YaKit/master/2014/exam00/task03/step04.sh)
+```
+7. На yakit-z03 выполните команду
+```
+yandex-tank
+```
+8. На yakit-z01 выполните команды
+```
+sudo su
+source <(curl -s https://raw.githubusercontent.com/KrylCW/YaKit/master/2014/exam00/task03/step06.sh)
+```
+9. Повторите шаги 1-3, 6, 8 на yakit-z02
+10. На yakit-z03 выполните команды
+```
+sudo su
+source <(curl -s https://raw.githubusercontent.com/KrylCW/YaKit/master/2014/exam00/task03/step07.sh)
+```
+11. По адресу yakit-z03 по HTTP будет доступен список отчетов yandex-tank'а 
+
+### Описание шагов
+1. На этом шаге 
--- 2014/exam00/task03/step03.sh
+++ 2014/exam00/task03/step03.sh
@@ -2,4 +2,51 @@
 
 update-grub  
 grub-install /dev/sda
-grub-install /dev/sdb  
+grub-install /dev/sdb
+
+# Install prerequisite
+apt-get install -y libapache2-mod-php5 php5-mysql mysql-server ssh
+
+# Install wordpress
+wget https://wordpress.org/latest.tar.gz
+tar xf latest.tar.gz -C /var/www/
+echo 'CREATE DATABASE wordpress' | mysql -u root -proot
+echo 'GRANT ALL PRIVILEGES ON wordpress.* TO "wordpress"@"localhost" IDENTIFIED BY "wordpress"' | mysql -u root -proot
+echo "
+<?php
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', 'wordpress');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+define('AUTH_KEY',         '_:6) bt;(3\$0E^D%vr%=&Fk)17.Jp^VLgw/7TXpE>3_CD1jH\$}n|*CZ&h[f@wJk^');
+define('SECURE_AUTH_KEY',  '_Dd\${3B5dLn#[_n/RB.bB.fs!( yduBaY5~6+/OK+>@. RmGAXq=^5~e<40]]-Ka');
+define('LOGGED_IN_KEY',    'HHKx~<\$x^ZBX LdEROQv{<Zy&:[>g59)vB6/Fa*#A(YF^>.XcRR[27I#P>?7iki7');
+define('NONCE_KEY',        'L\$A2vZpEZ@usR\$}q?%UVbJcBSh\$ln+\$Z;2?3L-rx%*< &+?RO=#UpB.fmvHE)52r');
+define('AUTH_SALT',        's|PtI=z:-o8^!X+@)jHu30MFJ]Ox%UGgr3~o=lNMv!kk+yB3|7Fu<Y3vZX3)+ZoO');
+define('SECURE_AUTH_SALT', '1RF(VO+Ca)\$.4]w}u%cuV=T#{05%~CagGXGrsjJl4Zy.q{>ZUa6^M _[!uM-wo^%');
+define('LOGGED_IN_SALT',   ' |\$ ZgQ?Ta k5\$M+I>i g<F/RlQDu-</LdU+z]G4I8^QRl~|P#PU*+|9w[qJ_;r*');
+define('NONCE_SALT',       'u3!h1,c0#d.cN(z 6DSDAah7x4a7S0UfQ]weZ^<vM4|@zRI)M|D}&:2k\$U_8* z^');
+\$table_prefix  = 'wp_';
+define('WP_DEBUG', false);
+if ( "\!"defined('ABSPATH') )
+     define('ABSPATH', dirname(__FILE__) . '/');
+require_once(ABSPATH . 'wp-settings.php');
+define('WP_HOME', 'http://10.0.2.101');
+define('WP_SITEURL', 'http://10.0.2.101');
+if ( ("\!"empty( \$_SERVER['HTTP_X_FORWARDED_HOST'])) ||
+     ("\!"empty( \$_SERVER['HTTP_X_FORWARDED_FOR'])) ) { 
+ 
+    // http://wordpress.org/support/topic/wordpress-behind-reverse-proxy-1
+    \$_SERVER['HTTP_HOST'] = \$_SERVER['HTTP_X_FORWARDED_HOST'];
+ 
+    define('WP_HOME', 'http://10.0.2.100:80');
+    define('WP_SITEURL', 'http://10.0.2.100:80');
+}
+" > /var/www/wordpress/wp-config.php
+sed -i 's#DocumentRoot /var/www/html#DocumentRoot /var/www/wordpress#' /etc/apache2/sites-available/000-default.conf
+chown www-data:www-data /var/www/wordpress
+ipaddr=`ifconfig eth0 | grep 'inet addr' | awk -F: '{print $2;}' | awk '{print $1;}'`
+sed -i "s#Listen 80#Listen $ipaddr:80#" /etc/apache2/ports.conf
+service apache2 restart
